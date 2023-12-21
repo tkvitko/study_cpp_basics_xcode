82b84f5acb6fe56ad9c3560e581d0eefd7e994e3
Taras Kvitko <tkvitko@gmail.com>
2023-12-21 07:41:59 +0300

    infinity game

--- race_game/race_game/Game.cpp
+++ race_game/race_game/Game.cpp
@@ -31,8 +31,8 @@ Game::Game(int distance_, int max_players_, SurfaceType type_) {
 }
 
 Game::~Game() {
-//    delete[] this->players;
-    this->clear_players();
+    delete[] this->players;
+//    this->clear_players();
 }
 
 std::string Game::get_game_info() {
@@ -65,10 +65,10 @@ Player* Game::get_players() {
     std::sort(this->players, this->players+players_number, &compare_results);
     return this->players;
 }
-
-void Game::clear_players() {
-    delete this->players;
-}
+//
+//void Game::clear_players() {
+//    delete[] this->players;
+//}
 
 void Game::play() {
     if (this->players_number < 2) {
